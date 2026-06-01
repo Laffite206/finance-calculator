ea0fd96c0fa78eee8c79f31b76b5dc4811a57340
Laffite206 <Lehaarapan10@gmail.com>
2026-06-02 02:08:11 +0700

    Реализовано добавление транзакций

--- js/app.js
+++ js/app.js
@@ -9,3 +9,47 @@ renderTransactions();
 
 // Подсчёт статистики
 updateStatistics();
+
+// Добавление транзакции
+document
+.getElementById("transactionForm")
+.addEventListener("submit",function(event){
+
+    event.preventDefault();
+
+    const amount =
+        Number(
+            document.getElementById("amount").value
+        );
+
+    if(amount <= 0){
+
+        showMessage(
+            "Введите корректную сумму",
+            "error"
+        );
+
+        return;
+    }
+
+    const transaction = {
+
+        id: Date.now(),
+
+        amount,
+
+        type:
+            document.getElementById("type").value,
+
+        category:
+            document.getElementById("category").value,
+
+        date:
+            document.getElementById("date").value,
+
+        comment:
+            document.getElementById("comment").value
+    };
+
+    addTransaction(transaction);
+});
